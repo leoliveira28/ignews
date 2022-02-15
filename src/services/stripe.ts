6e87851098d8512d958ba4c80ac6339a02089518
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-02-15 19:44:10 -0300

    environment variables

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,7 @@ import { version } from 'react'
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(
-    process.env.STRIPE_API_KEY,
+    process.env.NEXT_PUBLIC_PAYMENT_TOKEN,
     {
         apiVersion: '2020-08-27',
         appInfo: {
