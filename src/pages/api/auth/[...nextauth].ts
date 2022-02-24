d820cf25d9bd79e836535da0f30d98d3d7fda672
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-02-24 13:59:20 -0300

    final commmit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,7 +21,44 @@ export default NextAuth({
 //},
 
     callbacks: {
-      async signIn({ user, account, profile }) {
+      async session({session}) {
+        try {
+          const userActiveSubscription = await fauna.query(
+            q.Get(
+              q.Intersection([
+                q.Match(
+                  q.Index('subscription_by_user_ref'),
+                  q.Select(
+                    'ref',
+                    q.Get(
+                      q.Match(
+                        q.Index('user_by_email'),
+                        q.Casefold(session.user.email)
+                      )
+                    )
+                  )
+                ),
+                q.Match(
+                  q.Index('subscription_by_status'),
+                  'active'
+                )
+                ])
+            )
+          );
+          return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+        } catch {
+          return {
+            ...session,
+            activeSubscription: null,
+          }
+
+        }
+    },
+    
+      async signIn( {user, account, profile} ) {
         const { email } = user
         
         try {
@@ -30,7 +67,7 @@ export default NextAuth({
               q.Not(
                 q.Exists(
                   q.Match(
-                    q.Index('user_data_email'),
+                    q.Index('user_by_email'),
                     q.Casefold(user.email)
                   )
                 )
@@ -41,7 +78,7 @@ export default NextAuth({
               ),
               q.Get(
                 q.Match(
-                  q.Index('user_data_email'),
+                  q.Index('user_by_email'),
                   q.Casefold(user.email)
               )
             )
